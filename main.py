36815e8765e5a95d2b1baeea4ce07eca8273b7c2
Gitin18 <inka25618@gmail.com>
2024-10-03 23:18:52 +0200

    new picture

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("game tir")
-icon = pygame.image.load("https://www.google.com/interests/saved/list/snjwRX5rmtW2xDfqsaqtSyJAYB-gqQ?hl=ru&q=%D0%98%D0%B7%D0%B1%D1%80%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5+%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F&sa=X&ved=2ahUKEwjZifOt7PKIAxUt5r4IHbIBD68Q5oAKegQIARAf")
+icon = pygame.image.load("boez.jpg")
 pygame.display.set_icon(icon)
 
-##target_img = pygame.image.load("img/target.png")
+target_img = pygame.image.load("boez.jpg")
 target_width = 80
 target_height = 80
 
