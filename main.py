e0c8bbd1765be22f866e70b50f739a9629fb8889
Gitin18 <inka25618@gmail.com>
2024-10-04 23:54:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import random
 pygame.init()
 
 
+<<<<<<< Updated upstream
+=======
+pygame.display.set_caption("game tir")
+icon = pygame.image.load("boez.jpg")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("apfel.png")
+target_width = 80
+target_height = 80
+>>>>>>> Stashed changes
 
 
 
